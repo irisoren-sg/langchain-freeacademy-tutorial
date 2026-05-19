807a0528d5633f948c8c4e7d82441e99e5fe1ef6
irisoren-sg <iris.oren@gov.scot>
2026-05-19 11:13:39 +0000

    langchain_community -> langchain_ollama

--- module2/module2_tutorial_script1.py
+++ module2/module2_tutorial_script1.py
@@ -1,4 +1,4 @@
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama.chat_models import ChatOllama
 
 llm_llama = ChatOllama(
     model="llama3",     # or llama2, mistral, etc.
